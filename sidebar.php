dfd83833ae894dcd0abbcdf446d1d5dba40ce944
Dmytro Morykvas <morykvasd@gmail.com>
2024-04-10 16:22:38 +0300

    додав сайдбар

---
+++ sidebar.php
@@ -0,0 +1,9 @@
+
+
+<div class="container">
+    <div class="wrapper-sidebar">
+            <a href="">новини</a>
+            <a href="">рубрики</a>
+            <a href="">події</a>
+    </div>
+</div>
